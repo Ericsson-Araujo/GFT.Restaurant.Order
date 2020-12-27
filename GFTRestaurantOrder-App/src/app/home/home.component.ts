bf5d617b1ceeeb17345561d1af7c4f92b8779d31
Ericsson-Araujo <ericsson_araujo@hotmail.com>
2020-12-27 20:43:52 -0300

    refatoração de código e término da implementação de regras

--- GFTRestaurantOrder-App/src/app/home/home.component.ts
+++ GFTRestaurantOrder-App/src/app/home/home.component.ts
@@ -13,66 +13,84 @@ defineLocale('pt-br', ptBrLocale);
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
-  
+  // fields
   textOrder: string;
   textOutput: string;
   dishesHistorys: DishHistory[];
   filter: DishFilter;
 
+  // Constructor
   constructor(
-      private dishService: DishService      
+      private dishService: DishService
     ) {
-        this.dishesHistorys = [];        
+        this.dishesHistorys = [];
       }
-  
-  sendOrder() {    
+
+  // Begin Methods
+  sendOrder() {
     this.filter = new DishFilter();
     this.textOrder = this.textOrder.toLowerCase();
 
-    let arrayOrder = this.textOrder.split(',');
-    
+    const arrayOrder = this.textOrder.split(',');
+
     if (arrayOrder && arrayOrder.length > 1) {
         this.filter.timeOfDay = arrayOrder[0];
 
-        for (let i = 1; i < arrayOrder.length; i++) {          
-          this.filter.types.push(parseInt(arrayOrder[i]));
+        for (let i = 1; i < arrayOrder.length; i++) {
+          this.filter.types.push(parseInt(arrayOrder[i], 10));
         }
 
         this.searchDishes(this.filter);
     }
-    
+
   }
 
+  // Action's button 'Send Order'
   searchDishes(filter: DishFilter) {
-    
+
     this.dishService.SearchDishes(filter).subscribe(
       (_dishes: Dish[]) => {
-        this.textOutput = '';        
+        this.textOutput = '';
+        let validRepetition = true;
         let oldDish = new Dish();
         let cont = 1;
 
+        // loop for dishes
         for (let i = 0; i < _dishes.length; i++) {
           const dish = _dishes[i];
 
+          // organize output text
           if (i == 0)
             { this.textOutput = dish.description; }
-          else if (dish.description == oldDish.description)
-            { cont++; }
-          else if (cont > 1)
-            {              
+          else if (dish.description == oldDish.description) {
+            cont++;
+            if (i >= _dishes.length - 1)
+            {
               this.textOutput += ` (x${cont})`;
+              validRepetition = this.validateRepetition(oldDish, validRepetition);
+            }
+          }
+          else if (cont > 1)
+            {
+              this.textOutput += ` (x${cont})`;
+              validRepetition = this.validateRepetition(oldDish, validRepetition);
+              this.textOutput += ', ' + dish.description;
               cont = 1;
-              this.textOutput += ',' + dish.description;
             }
           else
-            { this.textOutput += ',' + dish.description; }
+            { this.textOutput += ', ' + dish.description; }
 
+          // set new value in oldDish var
           oldDish = dish;
-        }        
-      }, 
+        }
+
+        if (validRepetition === false) {
+          this.textOutput += ' INVALID ORDER (Only Cofee and potatoes can be repeated)';
+        }
+      },
       error => {
-        console.error(error);
         this.textOutput = `${error.status} ${error.error}`;
       }
     ).add(
@@ -81,17 +99,33 @@ export class HomeComponent implements OnInit {
       }
     );
   }
-  
+
+  // Add a row in History Grid
   addHistory() {
-    let history = new DishHistory();
+    const history = new DishHistory();
     history.input = this.textOrder;
     history.output = this.textOutput;
 
     this.dishesHistorys.unshift(history);
   }
 
+  validateRepetition(oldDish: Dish, validRepetition: bool): boolean {
+    if (!validRepetition){
+      return false;
+    }
+
+    if (oldDish.description.toLowerCase() == 'coffee'
+          || oldDish.description.toLowerCase() == 'potato') {
+      return true;
+    }
+    return false;
+  }
+
+  // End Methods
+
+  // Inicialize
   ngOnInit() {
-    
+
   }
 
 }
